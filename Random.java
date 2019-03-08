7dd4b2fdb123991e44a84b40c04bdf19ea5022d5
ahmedsilim0 <47721640+ahmedsilim0@users.noreply.github.com>
2019-03-08 20:15:36 +0200

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package random;
+
+/**
+ *
+ * @author ecs
+ */
+public class Random {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Random r= new Random();
+       System.out.println("Random vlaue is "+ r.nextInt());
+    }
+    
+}
